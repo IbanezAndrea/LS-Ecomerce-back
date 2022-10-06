66b4c521f0efb173894c1c463e5da43f4b3856c5
Elvio Galeano <elvio.galeano827@gmail.com>
2022-10-06 01:57:57 -0300

    controllers de update, getOne y delete

--- controllers/boxController.js
+++ controllers/boxController.js
@@ -73,7 +73,80 @@ const boxController = {
                 success: false
             })
         }
+    },
+    getOneBox: async (req,res) =>{
+        const {id} = req.params
+        try{
+            let box = await Box.findOne({_id:id})
+            if(box){
+                res.status(200).json({
+                    message: "you get one box",
+                    response: box,
+                    succes: true
+                })
+            }else{
+                res.status(404).json({
+                    message: "couldn't find box",
+                    succes: false
+            })
+        }
+        }catch(error){
+            console.log(error)
+            res.status(400).json({
+                message: "Error",
+                succes: false
+            })
+        }
+    },
+    updateBox: async(req,res)=>{
+        const {id} = req.params
+        const updateBox = req.body
+        try{
+            let box = await Box.findOneAndUpdate({_id:id}, updateBox, {new:true})
+            if(box){
+                res.status(200).json({
+                    message: "box updated successfully",
+                    response: box,
+                    succes: true
+                })
+            }else{
+                res.status(404).json({
+                    message: "couldn't find box",
+                    succes: false
+                })
+            }
+        }catch(error){
+            console.log(error)
+            res.status(400).json({
+                message: "error",
+                success: false,
+        })
+        }
+    },
+    deleteBox: async(req,res) =>{
+        const {id} = req.params
+        try{
+            let box = await Box.findOneAndDelete({_id:id})
+            if(box){
+                res.status(200).json({
+                    message: "box deleted successfully",
+                    success: true
+                  }) 
+            }else{
+                res.status(404).json({
+                    message: "couldn't find box",
+                    success: false,
+                })
+            }
+        }catch(error){
+            console.log(error)
+            res.status(400).json({
+                message: "error",
+                success: false,
+            })
+        }
     }
+
 
 }
 
