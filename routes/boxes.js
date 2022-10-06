03bd4ad2af74874ad91534ecbb6a649d9ffa1ef8
Elvio Galeano <elvio.galeano827@gmail.com>
2022-10-06 01:58:29 -0300

    rutas de los controllers de update, delete, getone

--- routes/boxes.js
+++ routes/boxes.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const {addBox, getAllBoxes} = require('../controllers/boxController');
+const {addBox, getAllBoxes,getOneBox,updateBox,deleteBox} = require('../controllers/boxController');
 
 
 router.post('/',addBox);
 router.get('/',getAllBoxes);
+router.get('/:id',getOneBox);
+router.put('/:id',updateBox); // no sabia si put o patch
+router.delete('/:id',deleteBox);
 
 module.exports = router
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const {addRecipe} = require('../controllers/recipeController');
+const {addRecipe,getAllRecipes,getOneRecipe,deleteRecipe} = require('../controllers/recipeController');
 
-router.post( '/', addRecipe);
+router.post('/', addRecipe);
+router.get('/:id', getAllRecipes);
+router.get('/:id', getOneRecipe);
+router.delete('/:id', deleteRecipe);
 
 module.exports = router;
