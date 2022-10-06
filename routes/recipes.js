e6f0560ec15ddd19b655fce4062be86e9757d660
Elvio Galeano <elvio.galeano827@gmail.com>
2022-10-06 12:49:37 -0300

    se puede escribir ñÑ en las contraseñas

--- routes/recipes.js
+++ routes/recipes.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const {addRecipe,getAllRecipes,getOneRecipe,deleteRecipe} = require('../controllers/recipeController');
 
 router.post('/', addRecipe);
-router.get('/:id', getAllRecipes);
+router.get('/', getAllRecipes);
 router.get('/:id', getOneRecipe);
 router.delete('/:id', deleteRecipe);
 
--- views/email.js
+++ views/email.js
@@ -1,6 +1,5 @@
 const email = (mail,code,url) => {
-    return `<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+    return `
 <body class="clean-body u_body" style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #bfedd2;color: #000000">
   <table id="u_body" style="border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;min-width: 320px;Margin: 0 auto;background-color: #bfedd2;width:100%" cellpadding="0" cellspacing="0">
     <tbody>
@@ -265,6 +264,6 @@ const email = (mail,code,url) => {
     </tbody>
   </table>
 </body>
-</html>`
+`
 }
 module.exports = email
