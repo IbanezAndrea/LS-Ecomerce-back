d9958fc8cae312d333ab0be8d8960e392124c298
juanPedroGalarza <juanpg232@gmail.com>
2022-10-11 15:35:01 -0300

    updateUserRouter

--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,16 @@ var express = require('express');
 var router = express.Router();
 let passport = require('../config/passport');
 let adminPassport = require('../config/adminPassport');
-const {userSignUp, verifyMail, userSignIn,userSignOut,getUser,getUsers,modifyUser,removeUser,verifyToken,updateAddresses} = require('../controllers/userController');
+const updateUserRouter = require('./updateUser');
+const {userSignUp, verifyMail, userSignIn,userSignOut,getUser,getUsers,verifyToken} = require('../controllers/userController');
 
 
 router.get('/',adminPassport.authenticate('jwt', { session: false }), getUsers);
-router.get('/:id', adminPassport.authenticate('jwt', { session: false }), getUser);
 router.post('/signup',userSignUp);
 router.post('/signin',userSignIn);
 router.post('/signout',passport.authenticate('jwt', { session: false }),userSignOut);
 router.get('/verify/:code',verifyMail);
 router.post('/verify-token',passport.authenticate('jwt', { session: false }),verifyToken);
-router.put('/update',passport.authenticate('jwt', { session: false }),modifyUser);
-router.patch('/update-address',passport.authenticate('jwt', { session: false }),updateAddresses);
-router.delete('/remove',passport.authenticate('jwt', { session: false }),removeUser);
+router.use('/update',updateUserRouter)
+router.get('/:id', getUser);
 module.exports = router;
