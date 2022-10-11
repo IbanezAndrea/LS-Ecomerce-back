5d1c8e9d232fb0c3dcb1b3aaaf2d4509acce81a7
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-10-11 13:02:34 -0300

    added passport to the route to create recipe

--- routes/recipes.js
+++ routes/recipes.js
@@ -4,7 +4,7 @@ let passport = require('../config/passport');
 let adminPassport = require('../config/adminPassport');
 const {addRecipe,getAllRecipes,getOneRecipe,deleteRecipe,approveRecipe, getRecipesByFilters} = require('../controllers/recipeController');
 
-router.post('/', addRecipe);
+router.post('/', passport.authenticate('jwt', { session: false }), addRecipe);
 router.patch('/approve/:id',adminPassport.authenticate('jwt', { session: false }), approveRecipe);
 router.get('/', getAllRecipes);
 router.get('/query/', getRecipesByFilters);
